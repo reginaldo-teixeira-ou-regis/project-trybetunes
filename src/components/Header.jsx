b65e9ed049e559bce8d5f9e9fd3507d09cb2954a
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-11-03 20:48:27 -0300

    Requisite 4 complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { NavLink, Switch } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
@@ -22,6 +23,30 @@ class Header extends Component {
     const { userName, loading } = this.state;
     return (
       <header data-testid="header-component">
+        <NavLink
+          className="link"
+          to="/search"
+          activeClassName="active"
+          data-testid="link-to-search"
+        >
+          Search
+        </NavLink>
+        <NavLink
+          className="link"
+          to="/favorites"
+          activeClassName="active"
+          data-testid="link-to-favorites"
+        >
+          Favorites
+        </NavLink>
+        <NavLink
+          className="link"
+          to="/profile"
+          activeClassName="active"
+          data-testid="link-to-profile"
+        >
+          Profile
+        </NavLink>
         Header
         { loading ? <p>Carregando...</p> : (
           <p data-testid="header-user-name">
